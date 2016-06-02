dc70dc367a2108eac998e02de6cfdf303c330290
James Tanner <tanner.jc@gmail.com>
2016-06-02 10:02:10 -0400

    One possible solution

--- test_foo.py
+++ test_foo.py
@@ -4,11 +4,14 @@ import unittest
 from mock import patch
 from lib.foo import foodoo
 
+def milkdud():
+    return "gotmilk?"
+
 class TestFoo(unittest.TestCase):
 
     def test_foodoo(self):
         assert foodoo() == None
 
-    # make this pass!
+    @patch('lib.foo.barnone', milkdud)
     def test_foodoo_withpatch(self):
         assert foodoo() == "gotmilk?"
